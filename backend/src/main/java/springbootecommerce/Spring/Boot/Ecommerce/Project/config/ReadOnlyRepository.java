11d69bec6a3f962092f107b9454830806c9f5b1f
JCano1497 <JCano1497@gmail.com>
2022-06-29 22:17:02 -0400

    Adding backend files

--- backend/src/main/java/springbootecommerce/Spring/Boot/Ecommerce/Project/config/ReadOnlyRepository.java
+++ backend/src/main/java/springbootecommerce/Spring/Boot/Ecommerce/Project/config/ReadOnlyRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface ReadOnlyRepository<T, ID> extends JpaRepository<T, ID> {
     Optional<T> findById(ID id);
     List<T> findAll();
+    //extended out the JPA rep so user wouldn't be able to make non reading actions
 }
