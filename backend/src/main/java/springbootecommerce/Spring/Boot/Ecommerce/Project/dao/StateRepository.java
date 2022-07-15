bcacd5350ad8c9e5ae68cf5fe5556820fe92a627
JCano1497 <JCano1497@gmail.com>
2022-07-14 20:29:01 -0400

    ReadOnly update

--- backend/src/main/java/springbootecommerce/Spring/Boot/Ecommerce/Project/dao/StateRepository.java
+++ backend/src/main/java/springbootecommerce/Spring/Boot/Ecommerce/Project/dao/StateRepository.java
@@ -3,11 +3,12 @@ package springbootecommerce.Spring.Boot.Ecommerce.Project.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import springbootecommerce.Spring.Boot.Ecommerce.Project.config.ReadOnlyRepository;
 import springbootecommerce.Spring.Boot.Ecommerce.Project.entity.State;
 
 import java.util.List;
 
 @CrossOrigin("http://localhost:4200")
-public interface StateRepository extends JpaRepository<State, Integer> {
+public interface StateRepository extends ReadOnlyRepository<State, Integer> {
   List<State> findByCountryCode(@Param("code") String code);
 }
